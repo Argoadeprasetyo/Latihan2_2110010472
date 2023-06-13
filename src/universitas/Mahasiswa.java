54dcecdb3051c3bbf6e3a720856474e9678f3a20
Argoadeprasetyo <adeargo123@gmail.com>
2023-06-13 09:06:06 +0800

    Membuat class Mahasiswa dan MahasiswaBeraksi

---
+++ src/universitas/Mahasiswa.java
@@ -0,0 +1,15 @@
+
+package universitas;
+
+
+public class Mahasiswa {
+    private String npm ;
+    private String nama ;
+    private String prodi ;
+  
+ 
+
+public String getDetail(){
+    return  npm + nama + prodi;
+}
+}
