98e01a0a233f44d59171f9309fd17abdb7bc56ce
Argoadeprasetyo <adeargo123@gmail.com>
2023-06-13 17:17:25 +0800

    Membuat class Mahasiswa dan MahasiswaBeraksi2

--- src/universitas/Mahasiswa.java
+++ src/universitas/Mahasiswa.java
@@ -1,15 +1,42 @@
-
 package universitas;
 
 
 public class Mahasiswa {
-    private String npm ;
-    private String nama ;
-    private String prodi ;
-  
- 
+  private String nama;
+  private String npm;
+  private String prodi;
 
-public String getDetail(){
-    return  npm + nama + prodi;
-}
+    public Mahasiswa(String npm, String nama, String prodi) {
+        this.nama = nama;
+        this.npm = npm;
+        this.prodi = prodi;
+    }
+  
+    public String setDetail(){
+        return npm + nama + prodi;
+    }
+
+    public String getProdi() {
+        return prodi;
+    }
+
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
+    }
+    
+    public String getNama() {
+        return nama;
+    }
+    
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+    public String getNpm() {
+        return npm;
+    }
+    
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
 }
--- src/universitas/MahasiswaBeraksi.java
+++ src/universitas/MahasiswaBeraksi.java
@@ -1,9 +1,17 @@
-
 package universitas;
 
 
 public class MahasiswaBeraksi {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+            
+    Mahasiswa mhs = new Mahasiswa("2110010472", " Argo Ade Prasetyo", " Teknik Informatika");
+    
+    
+        System.out.println("Nama: "+mhs.getNama());
+        System.out.println("NPM: "+mhs.getNpm());
+        System.out.println("Prodi: "+mhs.getProdi());
         
-    }
+        System.out.println(mhs.setDetail());
+
+}
 }
